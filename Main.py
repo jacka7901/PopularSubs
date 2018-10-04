63fccf987d7438566b4a44618cee9fc9eedd4d6a
Jack <jacka7901@gmail.com>
2018-10-03 22:25:58 -0400

    Second commit. Pandas DataFrame now being updated. Appearances have not yet been added

--- Main.py
+++ Main.py
@@ -1,68 +1,52 @@
 from selenium import webdriver
+import pandas as pd
 
 
 class Isduplicate:
     dicto = {}
 
-    def read(self):
-        f = open(r'C:\Users\jacka\OneDrive\Documents\outputs.txt', "r")
-
-        for line in f:
-            k, v = line.strip().split(':')
-            self.dicto[k.strip()] = int(v.strip())
-
-        return self.dicto
 
 
 
-Is = Isduplicate()
+    browser = webdriver.Chrome(executable_path=r'C:\Users\jacka\Downloads\chromedriver_win32\chromedriver.exe')
+
+    browser.get("https://www.reddit.com/")
+
+    #grabs the html tag for the subreddit name
+    elem = browser.find_elements_by_css_selector("a[data-click-id='subreddit']")
+
+    counter = 0
+
+    #output locations
+    text_file = open(r'C:\Users\jacka\OneDrive\Documents\outputs.csv', "a+")
+    Location = r'C:\Users\jacka\OneDrive\Documents\outputs.csv'
+
+    df = pd.read_csv(Location)
+    print(df)
+
+    print(len(elem))
+
+    while counter < 50:
+
+        #gets just the subreddit name
+        e = str(elem[counter].get_attribute("href"))
+        e = e.replace("https://www.reddit.com/r/", "")
+        e = e[:-1]
+
+        df = df.append({'Subreddit' : e}, ignore_index=True)
+
+        if e in df:
+            dicto[e] += 1
+        else:
+            dicto[e] = 1
+
+        #text_file.write(e + "," + str(dicto[e]) + '\n')
 
 
 
-browser = webdriver.Chrome(executable_path=r'C:\Users\jacka\Downloads\chromedriver_win32\chromedriver.exe')
+        print(e)
+        counter = counter + 2
 
-browser.get("https://www.reddit.com/")
-
-elem = browser.find_elements_by_css_selector("a[data-click-id='subreddit']")
-
-
-
-'''for line in Is.read():
-    k, v = line.split(":")
-    dicto[k] = v
-'''
-
-counter = 0
-
-
-text_file = open(r'C:\Users\jacka\OneDrive\Documents\outputs.txt', "a+")
-
-print(len(elem))
-
-while counter < 50:
-    e = str(elem[counter].get_attribute("href"))
-    e = e.replace("https://www.reddit.com/r/", "")
-    e = e[:-1]
-
-    if e in Is.read():
-        Is.dicto[e] += 1
-    else:
-        Is.dicto[e] = 1
-
-    text_file.write(e + ":" + str(Is.dicto[e]) + '\n')
-
-    '''if e in Is.read():
-        text_file.write("duplicate found")
-    else:
-        text_file.write(e + "\n")'''
-
-
-    print(e)
-    counter = counter +2
-
-print(Is.dicto)
-text_file.close()
-browser.close()
-
-
-
+    print(df)
+    text_file.close()
+    browser.close()
